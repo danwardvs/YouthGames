c1ac34ac8c276068247aaee00375c11f9a691a41
Danny Van Stemp <danwardvs@adsgames.net>
2020-05-23 10:20:07 -0400

    base project

---
+++ backend/dist/server.js
@@ -0,0 +1,8 @@
+"use strict";
+// src/server.ts
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.app = void 0;
+const ChatServer_1 = require("./ChatServer");
+let app = new ChatServer_1.ChatServer().app;
+exports.app = app;
+//# sourceMappingURL=server.js.map
