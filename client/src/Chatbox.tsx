85b106567c1bf7039104fb94b813d4c9c36e4755
Danny Van Stemp <danwardvs@adsgames.net>
2023-04-29 22:28:20 -0400

    env var for url

--- client/src/Chatbox.tsx
+++ client/src/Chatbox.tsx
@@ -1,21 +1,22 @@
 import React from "react";
 import "./App.css";
-import ScrollToBottom from "react-scroll-to-bottom";
 import { ChatMessage } from "./types";
 
 export const Chatbox: React.FC<{
   messages: ChatMessage[];
 }> = ({ messages }) => {
   return (
-    <ScrollToBottom className="App-chatbox">
-      {messages.slice(0, 5).map((msg: ChatMessage, index) => {
-        return (
-          <div key={index} className="App-chatbox-elem">
-            <p>{msg.author}</p>
-            <p>{msg.message}</p>
-          </div>
-        );
-      })}
-    </ScrollToBottom>
+    <div className="App-chatbox">
+      {messages
+        .slice(Math.max(messages.length - 4, 0))
+        .map((msg: ChatMessage, index) => {
+          return (
+            <div key={index} className="App-chatbox-elem">
+              <p>{msg.author}</p>
+              <p>{msg.message}</p>
+            </div>
+          );
+        })}
+    </div>
   );
 };
--- client/src/SocketService.ts
+++ client/src/SocketService.ts
@@ -6,9 +6,16 @@ export class SocketService {
   private socket: SocketIOClient.Socket = {} as SocketIOClient.Socket;
 
   public init(): SocketService {
-    console.log("initiating socket service");
-    this.socket = io("54.146.34.200:8080");
-    return this;
+    console.log("Initiating socket service");
+    console.log(process.env);
+    if (process.env.REACT_APP_SERVER_URL) {
+      console.log("Remote URL is " + process.env.REACT_APP_SERVER_URL);
+      this.socket = io(process.env.REACT_APP_SERVER_URL);
+      return this;
+    }
+
+    console.log("No remote URL found in environment variables.");
+    throw new Error("No remote URL found in environment variables.");
   }
 
   // send a message for the server to broadcast
